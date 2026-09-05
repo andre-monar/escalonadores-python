f1bd43910b5f3869fcb7dea3687f9eee9a7d2922
andre-monar <andre.m.garcia042@gmail.com>
2026-09-04 21:38:34 -0300

    feat(algoritmos): srtf, e correção de bug no sjf

--- algoritmos/srtf.py
+++ algoritmos/srtf.py
@@ -6,13 +6,14 @@ from models.tarefa import Tarefa
 
 from algoritmos._montar_resultado import montar_resultado
 
-def sjf(tarefas: list[Tarefa], ctx_time: float) -> ResultadoSimulacao:
 
+def srtf(tarefas: list[Tarefa], ctx_time: float) -> ResultadoSimulacao:
     periodos_por_tarefa: dict[int, list[Periodo]] = {tarefa.id: [] for tarefa in tarefas}
 
     tarefas_ordenadas = sorted(tarefas, key=lambda t: (t.chegada, t.id))
     tempo_atual = tarefas_ordenadas[0].chegada
     tarefas_pendentes = copy.deepcopy(tarefas_ordenadas)
+    ids_nao_finalizados = {tarefa.id for tarefa in tarefas_ordenadas}
     fila: list[Tarefa]= []
 
     def _encher_fila(tarefas_pendentes, tempo_atual, fila):
@@ -36,17 +37,31 @@ def sjf(tarefas: list[Tarefa], ctx_time: float) -> ResultadoSimulacao:
                 fila.insert(tarefa)
         return fila
 
-    while tarefas_pendentes:
-        # encher a fila com tarefas que chegaram até o tempo atual e estão fora dela
+    while ids_nao_finalizados:
+        # definir tarefa
         fila = _encher_fila(tarefas_pendentes, tempo_atual, fila)
-        
         if not fila:
             # se a fila estiver vazia, incrementa o tempo até a próxima tarefa chegar
             if tarefas_pendentes:
                 tempo_atual = min(tarefa.chegada for tarefa in tarefas_pendentes)
             continue
-
+        
         tarefa_atual = fila.pop(0)
+        # ir só até a próxima tarefa
+        # pegar próxima tarefa
+        proxima_tarefa = None
+        for tarefa in tarefas_ordenadas:
+            if tarefa.chegada > tempo_atual:
+                if proxima_tarefa is None or tarefa.chegada < proxima_tarefa.chegada:
+                    proxima_tarefa = tarefa
+
+        
+        final_previsto = tempo_atual + tarefa_atual.tp + ctx_time
+        tempo_incremental = tarefa_atual.tp
+        
+        if proxima_tarefa is not None and final_previsto > proxima_tarefa.chegada:
+            tempo_incremental = proxima_tarefa.chegada - tarefa_atual.chegada
+
         # add troca de contexto
         periodos_por_tarefa[tarefa_atual.id].append(Periodo(
             inicio=tempo_atual,
@@ -58,15 +73,17 @@ def sjf(tarefas: list[Tarefa], ctx_time: float) -> ResultadoSimulacao:
         # add execucao
         periodos_por_tarefa[tarefa_atual.id].append(Periodo(
             inicio=tempo_atual,
-            fim=tempo_atual + tarefa_atual.tp,
+            fim=tempo_atual + tempo_incremental,
             tipo=TipoPeriodo.EXECUCAO
         ))
-        tempo_atual += tarefa_atual.tp
-
-        # remover tarefa da lista de pendentes
-        tarefas_pendentes.remove(tarefa_atual)
+        tempo_atual += tempo_incremental
+        # deduzir tempo da tarefa
+        tarefa_atual.tp -= tempo_incremental
+        if tarefa_atual.tp <= 0:
+            ids_nao_finalizados.remove(tarefa_atual.id)
+            tarefas_pendentes.remove(tarefa_atual)
 
     return montar_resultado(
         tarefas, periodos_por_tarefa,
-        algoritmo="SJF", ctx_time=ctx_time,
+        algoritmo="SRTF", ctx_time=ctx_time,
     )
